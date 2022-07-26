f696803c637c0a26b5681ad0f3e33902faa7a6d8
DariaOsip <osipooov@bk.ru>
2022-07-26 21:18:02 +0200

    secret

--- src/Key.js
+++ src/Key.js
@@ -4,7 +4,7 @@ const Key = ({letter}) => {
     return <div className="key">
         {
             letter.isShown && <Slide direction="down" in={true} appear={true} timeout={1000}>
-                <p style={{ verticalAlign: 'center' }}>{letter.letter}</p>
+                <span style={{ verticalAlign: 'center' }}>{letter.letter}</span>
             </Slide>
         }
     </div>;
